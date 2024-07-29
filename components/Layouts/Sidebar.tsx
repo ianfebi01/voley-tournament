90e64b2541ca231c3762980d2e8c8b6cae9d2b83
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2024-07-29 16:28:00 +0700

    chore: change sidebar menu

--- components/Layouts/Sidebar.tsx
+++ components/Layouts/Sidebar.tsx
@@ -1,7 +1,6 @@
-'use client'
-import { faBusinessTime, faCode, faPenRuler, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
+'use client';
+import { faGamepad, faVolleyball } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image'
-import React from 'react'
 import ButtonSignOut from '../Buttons/ButtonSignOut'
 import { useSession } from 'next-auth/react'
 import ButtonSidebar from '../Buttons/ButtonSidebar'
@@ -13,67 +12,49 @@ const Sidebar = () => {
   return (
     <aside
       id="sidebar"
-      className="fixed top-0 left-0 z-40 w-64 h-screen py-7 transition-transform sm:translate-x-0 bg-dark-secondary flex flex-col"
+      className="fixed top-0 left-0 z-40 flex flex-col w-64 h-screen transition-transform py-7 sm:translate-x-0 bg-dark-secondary"
       aria-label="Sidebar"
     >
-      <div className=" mb-6 pb-6 mx-3 flex items-center gap-4 border-b border-white/25">
+      <div className="flex items-center gap-4 pb-6 mx-3 mb-6 border-b border-white/25">
         <Image src="/Logo.svg"
           alt="Logo image"
           width={40}
           height={40}
         />
         <div className="flex gap-2">
-          <h1 className="leading-none m-0 text-2xl font-bold">IAN</h1>
-          <h1 className="text-orange leading-none m-0 text-2xl font-bold">
-            FEBI
+          <h1 className="m-0 text-xl font-bold leading-none">SEMULUH</h1>
+          <h1 className="m-0 text-xl font-bold leading-none text-orange">
+            KIDUL
           </h1>
         </div>
       </div>
       <div className="grow-[1] px-3 pb-4 overflow-y-auto">
         <ul className="space-y-2">
           <li>
-            <ButtonSidebar text='Profile'
-              icon={faUser}
+            <ButtonSidebar text='Team'
+              icon={faVolleyball}
               path="/admin"
             />
           </li>
           <li>
-            <ButtonSidebar text='Portofolio'
-              icon={faCode}
-              path="/admin/portofolio"
-            />
-          </li>
-          <li>
-            <ButtonSidebar text='Position'
-              icon={faBusinessTime}
-              path="/admin/position"
-            />
-          </li>
-          <li>
-            <ButtonSidebar text='Skill'
-              icon={faPenRuler}
-              path="/admin/skill"
-            />
-          </li>
-          <li>
-            <ButtonSidebar text='Experience'
-              icon={faSuitcase}
-              path="/admin/experience"
+            <ButtonSidebar text='Games'
+              icon={faGamepad}
+              path="/admin/games"
             />
           </li>
         </ul>
       </div>
       <div className="px-3">
         <div>
-          <div className='bg-transparent hover:bg-dark border border-white/25 hover:border-transparent p-2 transition-all ease-in-out duration-500 text-base text-white flex items-center gap-2 w-full rounded-lg'>
+          <div className='flex items-center w-full gap-2 p-2 text-base text-white transition-all duration-500 ease-in-out bg-transparent border rounded-lg hover:bg-dark border-white/25 hover:border-transparent'>
             <Image src={session?.user?.avatar as string}
               alt='Avatar'
               width={40}
               height={40}
-              className='border border-none rounded-full overflow-hidden'
+              className='overflow-hidden border border-none rounded-full'
             />
             <div className='flex flex-col gap-2'>
-              <h2 className='line-clamp-1 text-xs'>{session?.user?.name}</h2>
+              <h2 className='text-xs line-clamp-1'>{session?.user?.name}</h2>
               <p className='line-clamp-1 text-[0.5rem] leading-none'>{session?.user?.email}</p>
             </div>
             <ButtonSignOut iconOnly/>
